c76808ebd13963107632e74c96890178f8215fb5
LuLiu1004 <luliu1004@163.com>
2025-10-13 09:58:30 +0800

    proj1b: draft completed

---
+++ proj1b/Palindrome.java
@@ -0,0 +1,63 @@
+public class Palindrome {
+    /**
+     * Given a String, wordToDeque should return a Deque
+     * where the characters appear in the same order as in the String.
+     * @param word the given String
+     * @return a Deque where the characters appear in the same order as in the String.
+     */
+    public Deque<Character> wordToDeque(String word) {
+        if (word == null) {
+            System.out.println("The input word cannot be null.");
+            return null;
+        }
+        Deque<Character> wordDeque = new LinkedListDeque<>();
+        for (int i = 0; i < word.length(); i++) {
+            wordDeque.addLast(word.charAt(i));
+        }
+        return wordDeque;
+
+    }
+
+    /**
+     * Verify if the given word is a palindrome.
+     * @param word the given word
+     * @return true if the given word is a palindrome, and false otherwise.
+     */
+    public boolean isPalindrome(String word) {
+        if (word == null || word.length() <= 1) {
+            return true;
+        }
+
+        Deque<Character> wordDeque = wordToDeque(word);
+        int numOfIterations = wordDeque.size() / 2;
+        for (int i = 0; i < numOfIterations; i++) {
+            Character firstChar = wordDeque.removeFirst();
+            Character lastChar = wordDeque.removeLast();
+            if (!firstChar.equals(lastChar)) {
+                return false;
+            }
+        }
+        return  true;
+    }
+
+    public boolean isPalindrome(String word, CharacterComparator cc) {
+        if (word == null || word.length() <= 1) {
+            return true;
+        }
+
+
+        Deque<Character> wordDeque = wordToDeque(word);
+        int numOfIterations = wordDeque.size() / 2;
+        for (int i = 0; i < numOfIterations; i++) {
+            Character firstChar = wordDeque.removeFirst();
+            Character lastChar = wordDeque.removeLast();
+            if (!cc.equalChars(firstChar, lastChar)) {
+                return false;
+            }
+        }
+        return  true;
+    }
+
+
+
+}
--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestPalindrome {
-    /*// You must use this palindrome, and not instantiate
+    // You must use this palindrome, and not instantiate
     // new Palindromes, or the autograder might be upset.
     static Palindrome palindrome = new Palindrome();
 
@@ -14,5 +14,27 @@ public class TestPalindrome {
             actual += d.removeFirst();
         }
         assertEquals("persiflage", actual);
-    } Uncomment this class once you've created your Palindrome class. */
+    }
+    @Test
+    public void testIsPalindrome() {
+        // normal cases
+        assertTrue(palindrome.isPalindrome("noon"));
+        assertTrue(palindrome.isPalindrome("racecar"));
+        assertFalse(palindrome.isPalindrome("cat"));
+        // corner cases
+        assertTrue(palindrome.isPalindrome("a"));
+        assertFalse(palindrome.isPalindrome("aA"));
+        assertTrue(palindrome.isPalindrome(null));
+
+        // test isPalindrome with CharacterComparator offByOne
+        CharacterComparator ob1 = new OffByOne();
+        assertTrue(palindrome.isPalindrome("flake", ob1));
+
+        // test isPalindrome with CharacterComparator offBy5
+
+        CharacterComparator offBy5 = new OffByN(5);
+        assertTrue(palindrome.isPalindrome("toto", offBy5));
+
+
+    }
 }
