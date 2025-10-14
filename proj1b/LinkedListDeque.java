81ae369e53976d33647d4ab58f3283821858b267
LuLiu1004 <luliu1004@163.com>
2025-10-14 09:00:59 +0800

    check for styles

--- proj1b/LinkedListDeque.java
+++ proj1b/LinkedListDeque.java
@@ -1,11 +1,10 @@
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
-
 /**
  * Isn't this solution kinda... cheating? Yes.
  */
 public class LinkedListDeque<Item> extends LinkedList<Item> implements Deque<Item> {
-    
+
     @Override
     public void printDeque() {
         System.out.println("dummy");
